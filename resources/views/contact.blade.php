f841b5a1e582af30b74588493b81d069393da098
a <a>
2023-09-05 16:42:05 -0600

    contact view

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <form action = "#">
+        <label for = "email">Email</label>
+        <input type = "email" name = "email"><br>
+        <label for = "comment">Comment</label>
+        <textarea name = "comment" cols = "30" rows = "10"></textarea>
+        <input type = "submit" value = "submit">
+    </form>
+</body>
+</html>
